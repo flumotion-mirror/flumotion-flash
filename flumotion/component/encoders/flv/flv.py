f757bcbf4799676d8045b4b365104b149849711a
Julien Moutte <julien@moutte.net>
2006-12-29 15:40:40 +0000

    Adding flumotion-flash project
    svn path=/flumotion-advanced/flumotion-flash/; revision=4726

---
+++ flumotion/component/encoders/flv/flv.py
@@ -0,0 +1,24 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+from flumotion.component import feedcomponent
+
+class FLVEncoder(feedcomponent.ParseLaunchComponent):
+    def get_pipeline_string(self, properties):
+        return "ffmpegcolorspace ! ffenc_flv name=encoder" 
+
+    def configure_pipeline(self, pipeline, properties):
+        element = pipeline.get_by_name('encoder')
+        if properties.has_key('bitrate'):
+            element.set_property('bitrate', properties['bitrate'])
---
+++ flumotion/component/muxers/flv/flv.py
@@ -0,0 +1,36 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+import gst
+
+from flumotion.component import feedcomponent
+
+class FLVMuxer(feedcomponent.ParseLaunchComponent):
+    def get_pipeline_string(self, properties):
+        pipeline = 'fluflvmux broadcast=true name=muxer '
+
+        for eater in self.config['source']:
+            if gst.gst_version < (0, 9):
+                pipeline += '{ @ eater:%s @ ! queue max-size-buffers=16 } ! muxer. '\
+                    % eater
+            else:
+                pipeline += '@ eater:%s @ ! queue max-size-buffers=16 ! muxer. '\
+                    % eater
+
+        pipeline += 'muxer.'
+
+        return pipeline
+    
+    def configure_pipeline(self, pipeline, properties):
+        element = pipeline.get_by_name('muxer')
