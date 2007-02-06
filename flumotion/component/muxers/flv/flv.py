d54cb6ed8672eed8e5df5bc2943595a1b13924ea
Michael Smith <msmith@fluendo.com>
2007-02-06 11:28:20 +0000

    * flumotion/component/muxers/flv/flv.py:           Change FLV muxer to use MultiInputParseLaunchComponent, so this           isn't deadlock-prone.
    svn path=/flumotion-advanced/flumotion-flash/; revision=5047

--- flumotion/component/muxers/flv/flv.py
+++ flumotion/component/muxers/flv/flv.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 #
 # Flumotion - a streaming media server
-# Copyright (C) 2004,2005,2006 Fluendo, S.L. (www.fluendo.com).
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
 # All rights reserved.
 
 # Licensees having purchased or holding a valid Flumotion Advanced
@@ -12,25 +12,9 @@
 
 # Headers in this file shall remain intact.
 
-import gst
-
 from flumotion.component import feedcomponent
 
-class FLVMuxer(feedcomponent.ParseLaunchComponent):
-    def get_pipeline_string(self, properties):
-        pipeline = 'fluflvmux broadcast=true name=muxer '
+class FLVMuxer(feedcomponent.MultiInputParseLaunchComponent):
+    def get_muxer_string(self, properties):
+        return 'fluflvmux broadcast=true name=muxer'
 
-        for eater in self.config['source']:
-            if gst.gst_version < (0, 9):
-                pipeline += '{ @ eater:%s @ ! queue max-size-buffers=16 } ! muxer. '\
-                    % eater
-            else:
-                pipeline += '@ eater:%s @ ! queue max-size-buffers=16 ! muxer. '\
-                    % eater
-
-        pipeline += 'muxer.'
-
-        return pipeline
-    
-    def configure_pipeline(self, pipeline, properties):
-        element = pipeline.get_by_name('muxer')
