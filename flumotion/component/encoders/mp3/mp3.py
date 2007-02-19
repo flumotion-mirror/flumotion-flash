6ff7414f46ebb7afc71570f1f3feca266def832e
Thomas Vander Stichele <thomas@apestaart.org>
2007-02-19 18:21:22 +0000

    * flumotion/component/encoders/mp3/mp3.py: 	  Remove unneeded if statement. 	  Add audiorate, to compensate for lame's inability to deal with 	  dropped audio frames until lame gets fixed. 	  Tested with flumotion-launch audiotest-producer ! mp3-encoder ! 	  flv-muxer ! disk-consumer directory=. rotate-type=none 	  to verify that it negotiates correctly.
    svn path=/flumotion-advanced/flumotion-flash/trunk/; revision=5234

--- flumotion/component/encoders/mp3/mp3.py
+++ flumotion/component/encoders/mp3/mp3.py
@@ -16,10 +16,8 @@ from flumotion.component import feedcomponent
 
 class MP3Encoder(feedcomponent.ParseLaunchComponent):
     def get_pipeline_string(self, properties):
-        if properties.has_key('rate'):
-            return "audioconvert ! audioresample ! audio/x-raw-int,rate=%d ! lame name=encoder ! mp3parse" % properties['rate']
-        else:
-            return "audioconvert ! audioresample ! audio/x-raw-int,rate=44100 ! lame name=encoder ! mp3parse"
+        rate = properties.get('rate', 44100)
+        return "audioconvert ! audioresample ! audio/x-raw-int,rate=%d ! audiorate ! lame name=encoder ! mp3parse" % rate
 
     def configure_pipeline(self, pipeline, properties):
         element = pipeline.get_by_name('encoder')
