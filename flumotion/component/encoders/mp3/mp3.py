34f645287b55a7cfad2c7da311d48942e4f531e3
Thomas Vander Stichele <thomas@apestaart.org>
2007-10-03 17:05:03 +0000

    * flumotion/component/encoders/mp3/mp3.py: 	  Bitrate should be specified in bit/sec. 	  Add a warning message if it is specified wrongly so admins 	  can fix their config, but still accept it for now.
    svn path=/flumotion-advanced/flumotion-flash/trunk/; revision=8763

--- flumotion/component/encoders/mp3/mp3.py
+++ flumotion/component/encoders/mp3/mp3.py
@@ -14,6 +14,11 @@
 
 from flumotion.component import feedcomponent
 
+from flumotion.common import messages
+
+N_ = messages.N_
+T_ = messages.gettexter('flumotion-flash')
+
 class MP3Encoder(feedcomponent.ParseLaunchComponent):
     checkTimestamp = True
     checkOffset = True
@@ -23,6 +28,29 @@ class MP3Encoder(feedcomponent.ParseLaunchComponent):
         return "audioconvert ! audioresample ! audio/x-raw-int,rate=%d ! lame name=encoder ! mp3parse" % rate
 
     def configure_pipeline(self, pipeline, properties):
+        self.debug('configure_pipeline')
         element = pipeline.get_by_name('encoder')
-        if properties.has_key('bitrate'):
-            element.set_property('bitrate', properties['bitrate'])
+        bitrate = properties.get('bitrate')
+        if not bitrate:
+            self.debug('No bitrate set, using default')
+            return
+
+        # the lame element accepts bitrate in kbits/sec, integer
+        # Flumotion handles bitrate in bits/sec, but since there are
+        # wrong configs, we accept both and convert as needed
+        if bitrate < 1000:
+            # assume bitrate is specified in kbit/sec
+            m = messages.Warning(
+                T_(N_("The configuration specifies the bitrate in kbit/sec "
+                    "with a value of '%d'. "
+                    "Please update the configuration and specify "
+                    "bitrate in bit/sec with a value of '%d'."), bitrate,
+                    bitrate * 1000))
+            self.addMessage(m)
+            bitrate *= 1000
+
+        # now we have it in bit/sec, convert to lame's idea
+        bitrate = int(bitrate / 1000)
+
+        self.debug('setting bitrate to %d' % bitrate)
+        element.set_property('bitrate', bitrate)
